8b8ee8f1c1e4f99626f23c96de4f01970db67b1f
IlBuonTommy <tomamsobianchin23@gmail.com>
2021-05-22 11:46:30 +0200

    New tamatriste

--- src/it/unibs/ing/fp/lab/TamaZoo/Tamatriste.java
+++ src/it/unibs/ing/fp/lab/TamaZoo/Tamatriste.java
@@ -1,15 +1,36 @@
 package it.unibs.ing.fp.lab.TamaZoo;
 
 public class Tamatriste extends Tamagotchi{
+	
+	private static final int MAX_SAZIETA = 100;
+	private static final String TRISTE = ":(";
+	private static final int MIN_SODDISFAZIONE = 0;
 
-	public Tamatriste(String nome, double soddisfazione, double sazieta) {
-		super(nome, soddisfazione, sazieta);
+	public Tamatriste(String nome, double sazieta) {
+		super(nome, MIN_SODDISFAZIONE, sazieta);
 		
 	}
 	
 	@Override
-	public void riceviCarezze() {
+	public boolean sonoTriste() {
+
+		return true;
+	}
+	
+	@Override
+	public boolean sonoMorto( ) {
+
+		if(super.getGradoSazieta() == 0 || super.getGradoSazieta() == MAX_SAZIETA){
+
+			return true;
+		}
+
+		return false;
+	}
+	
+	@Override
+	public String felicita() {	
+		return TRISTE;
 		
 	}
-
 }
