e2cc845a5b1d64661090adb13572a61a73e85a25
Brignoo <gianlu.brignoli@gmail.com>
2021-05-22 10:43:45 +0200

    --

---
+++ src/it/unibs/ing/fp/lab/TamaZoo/Tamatriste.java
@@ -0,0 +1,10 @@
+package it.unibs.ing.fp.lab.TamaZoo;
+
+public class Tamatriste extends Tamagotchi{
+
+	public Tamatriste(String nome, double soddisfazione, double sazieta) {
+		super(nome, soddisfazione, sazieta);
+		// TODO Auto-generated constructor stub
+	}
+
+}
