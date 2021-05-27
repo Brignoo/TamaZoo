c2a73e1d1d7eed02cc282bd6781f2b427ececcb0
Brignoo <gianlu.brignoli@gmail.com>
2021-05-27 15:39:54 +0200

    --

--- src/it/unibs/ing/fp/lab/TamaZoo/TamaZoo.java
+++ src/it/unibs/ing/fp/lab/TamaZoo/TamaZoo.java
@@ -1,5 +1,13 @@
 package it.unibs.ing.fp.lab.TamaZoo;
 
+import java.util.ArrayList;
+
 public class TamaZoo {
 
+	private ArrayList<Tamagotchi> tama;
+	
+	public TamaZoo() {
+
+		this.tama = new ArrayList<Tamagotchi>();
+	}
 }
