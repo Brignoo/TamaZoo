17c14a94df74a8d379bccd732acf61a33e08b47d
Gioelio <Mattiazan00i@gmail.com>
2021-06-06 20:36:11 +0200

    programma completo

--- src/it/unibs/ing/fp/lab/TamaZoo/TamaMain.java
+++ src/it/unibs/ing/fp/lab/TamaZoo/TamaMain.java
@@ -1,6 +1,10 @@
 package it.unibs.ing.fp.lab.TamaZoo;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import it.unibs.ing.fp.lab.mylib.BelleStringhe;
 import it.unibs.ing.fp.lab.mylib.EstrazioniCasuali;
 import it.unibs.ing.fp.lab.mylib.MyInputDati;
 
@@ -9,38 +13,40 @@ import it.unibs.ing.fp.lab.mylib.MyInputDati;
  *
  */
 public class TamaMain {
-	
+
 	private static final String SALUTO = "Benvenuto nel Tamazoo\n\n";
 	private static final String NUMERO_TAMAGOTCHI = "Quanti tamagotchi vuoi aggiungere? ";
 	private static final String INSERIMENTO_TAMAGOTCHI = "Inserisci il nome del tuo tamagotchi: ";
-	
+	private static final String FINE_PER_MORTE = "Tutti i tamagotchi sono morti";
+
 	private static final int MINIMO_NUM_TAMAGOTCHI = 1;
 	private static final int MINIMO_ESTRAZIONE = 0;
 	private static final int MASSIMO_ESTRAZIONE = 2;
 	private static final int MINIMO_LIVELLO = 20;
 	private static final int MASSIMO_LIVELLO = 80;
-	
+
 	private static void saluta() {
-		
+
 		System.out.println(SALUTO);
 	}
-	
+
 	/**
-	 * Metodo per la creazione di un nuovo Tamagotchi con scelta casuale del tipo e determinazione 
-	 * casuale dei valori di sazietà e soddisfazione
+	 * Metodo per la creazione di un nuovo Tamagotchi con scelta casuale del tipo e
+	 * determinazione casuale dei valori di sazietà e soddisfazione
+	 * 
 	 * @param nome nome del tamagotchi
 	 * @return ritorna il nuovo tamagotchi
 	 */
 	private static Tamagotchi creaTamagotchi(String nome) {
-		
+
 		Tamagotchi t = null;
 		int num, sazieta, soddisfazione;
-		
+
 		num = EstrazioniCasuali.estraiIntero(MINIMO_ESTRAZIONE, MASSIMO_ESTRAZIONE);
 		sazieta = EstrazioniCasuali.estraiIntero(MINIMO_LIVELLO, MASSIMO_LIVELLO);
 		soddisfazione = EstrazioniCasuali.estraiIntero(MINIMO_LIVELLO, MASSIMO_LIVELLO);
-		
-		switch(num) {
+
+		switch (num) {
 			case 0:
 				t = new Tamagotchi(nome, soddisfazione, sazieta);
 				break;
@@ -51,39 +57,76 @@ public class TamaMain {
 				t = new TamaTriste(nome, soddisfazione);
 				break;
 		}
-		
+
 		return t;
 	}
-	
+
 	/**
-	 * Metodo per popolare le lista di Tamagotchi, a partire dal numero di tamagotchi
-	 * li aggiunge alla lista con il nome scelto dall'utente
+	 * Metodo per popolare le lista di Tamagotchi, a partire dal numero di
+	 * tamagotchi li aggiunge alla lista con il nome scelto dall'utente
+	 * 
 	 * @param tamazoo
 	 */
-	private static void popolaLista( ArrayList<Tamagotchi> tamazoo) {
-		
+	private static void popolaLista(ArrayList<Tamagotchi> tamazoo) {
+
 		String nome;
 		int num;
 		num = MyInputDati.leggiInteroConMinimo(NUMERO_TAMAGOTCHI, MINIMO_NUM_TAMAGOTCHI);
-		
-		for( int i = 0; i < num; i++ ) {
-			
+
+		for (int i = 0; i < num; i++) {
+
 			nome = MyInputDati.leggiStringa(INSERIMENTO_TAMAGOTCHI);
-			tamazoo.add( creaTamagotchi(nome) );
+			tamazoo.add(creaTamagotchi(nome));
 		}
 	}
 
 	public static void main(String[] args) {
-		
+
 		ArrayList<Tamagotchi> tamaZoo = new ArrayList<Tamagotchi>();
-		int num;
-		String nome;
-		
+
 		saluta();
 		popolaLista(tamaZoo);
-		
-		
-		
+
+		boolean esci = false;
+		Scanner s = new Scanner(System.in);
+
+		while (!esci) {
+			// out.... scegli un'opzione lista opzioni
+			System.out.println("Scegli un'opzione\n0:Esci\n1: Carezze\n2: Biscotti");
+			int scelta = s.nextInt();
+
+			switch (scelta) {
+				case 0:
+					esci = true;
+					break;
+				case 1:
+					tamaZoo.forEach((t) -> {
+						t.riceviBiscotti();
+					});
+					break;
+				case 2:
+					tamaZoo.forEach((t) -> {
+						t.riceviCarezze();
+					});
+					break;
+			}
+
+			for (Iterator<Tamagotchi> iter = tamaZoo.iterator(); iter.hasNext();) {
+				Tamagotchi t = iter.next();
+
+				if (t.sonoMorto())
+					iter.remove();
+				else
+					System.out.println(t.toString());
+			}
+
+			if (tamaZoo.size() == 0) {
+				esci = true;
+				System.out.println(BelleStringhe.incorniciaECentra(FINE_PER_MORTE) + BelleStringhe.centrata(":("));
+			}
+		}
+
+		s.close();
 	}
 
 }
--- src/it/unibs/ing/fp/lab/TamaZoo/Tamagotchi.java
+++ src/it/unibs/ing/fp/lab/TamaZoo/Tamagotchi.java
@@ -5,166 +5,186 @@ import it.unibs.ing.fp.lab.mylib.EstrazioniCasuali;
 
 /**
  * <p>
- * La classe <strong>Tamagotchi</strong> contiene tute le informazioni del tamagotchi
- * al suo interno si trovano i metodi per modificare lo stato del tamagotchi
+ * La classe <strong>Tamagotchi</strong> contiene tute le informazioni del
+ * tamagotchi al suo interno si trovano i metodi per modificare lo stato del
+ * tamagotchi
  * </p>
  * 
  * @version 1.0
  */
 
 public class Tamagotchi {
-	
+
 	private static final int MAX_SAZIETA = 100;
 	private static final int MIN_SAZIETA = 0;
 	private static final int MAX_SODDISFAZIONE = 100;
 	private static final int MIN_SODDISFAZIONE = 0;
-	
+
 	private static final String FELICE = ":)";
 	private static final String INDIFFERENTE = ":|";
 	private static final String TRISTE = ":(";
-	
+
 	private static final int TRISTEZZA_SAZIETA = 30;
 	private static final int TRISTEZZA_SODDISFAZIONE = 30;
 	private static final int TRISTEZZA_SAZIETA_MAX = 90;
 
 	private static final int MAX_AZIONE = 15;
 	private static final int MIN_AZIONE = 5;
-	
+
 	/**
-	 * Indica il <strong>grado di soddisfazione</strong> costantemente aggiornato del Tamagotchi
+	 * Indica il <strong>grado di soddisfazione</strong> costantemente aggiornato
+	 * del Tamagotchi
 	 */
 	private double gradoAffettivo;
 	/**
-	 * Indica il <strong>grado di sazietà</strong> costantemente aggiornato del Tamagotchi
+	 * Indica il <strong>grado di sazietà</strong> costantemente aggiornato del
+	 * Tamagotchi
 	 */
 	private double gradoSazieta;
 	/**
 	 * Indica il <strong>nome</strong> del Tamagotchi
 	 */
 	private String nome;
-	
+
 	/**
 	 * Crea un oggetto della classe <strong>Tamagotchi</strong> specificando il
-	 * <em>nome</em>, la <em>sazietà</em> e la <em>soddisfazione</em> ricevuti dalla classe
-	 * <a href="TamaMain.html">TamaMain</a>.
+	 * <em>nome</em>, la <em>sazietà</em> e la <em>soddisfazione</em> ricevuti dalla
+	 * classe <a href="TamaMain.html">TamaMain</a>.
 	 * 
-	 * @param nome				Indica il <em>nome</em> del Tamagotchi
-	 * @param sazieta			Indica il grado iniziale di <em>sazietà</em> del Tamagotchi
-	 * @param soddisfazione		Indica il grado iniziale di <em>soddisfazione</em> del Tamagotchi
+	 * @param nome          Indica il <em>nome</em> del Tamagotchi
+	 * @param sazieta       Indica il grado iniziale di <em>sazietà</em> del
+	 *                      Tamagotchi
+	 * @param soddisfazione Indica il grado iniziale di <em>soddisfazione</em> del
+	 *                      Tamagotchi
 	 */
-	public Tamagotchi( String nome, double soddisfazione, double sazieta ) {
-		
+	public Tamagotchi(String nome, double soddisfazione, double sazieta) {
+
 		this.nome = nome;
 		this.gradoAffettivo = soddisfazione;
 		this.gradoSazieta = sazieta;
-		
-		if ( !controlloValori() ) {
-			
-			throw new IllegalArgumentException(); 
+
+		if (!controlloValori()) {
+
+			throw new IllegalArgumentException();
 		}
 	}
 
 	/**
 	 * Restituisce il <strong>nome</strong> del <em>Tamagotchi</em>
 	 * 
-	 * @return Una <code>String</code> rappresentante il <strong>nome</strong> del <em>Tamagotchi</em>
+	 * @return Una <code>String</code> rappresentante il <strong>nome</strong> del
+	 *         <em>Tamagotchi</em>
 	 */
 	public String getNome() {
 		return nome;
 	}
-	
+
 	/**
-	 * Restituisce un <strong>numero casuale</strong> compreso tra <em>MIN_AZIONE</em> e <em>MAX_AZIONE</em>
+	 * Restituisce un <strong>numero casuale</strong> compreso tra
+	 * <em>MIN_AZIONE</em> e <em>MAX_AZIONE</em>
 	 * 
-	 * @return Il numero casuale di tipo <code>int</code> estratto dalla funzione <em>EstrazioniCasuali.estraiIntero</em>
+	 * @return Il numero casuale di tipo <code>int</code> estratto dalla funzione
+	 *         <em>EstrazioniCasuali.estraiIntero</em>
 	 */
-	private static int getRandomInt( ) {
+	private static int getRandomInt() {
 
 		return EstrazioniCasuali.estraiIntero(MIN_AZIONE, MAX_AZIONE);
 	}
 
 	/**
-	 * Esegue il return del <strong>numero minimo</strong> dei due passati
-	 *  come parametri, usando la funzione <em>Math.min()</em>
-	 *  
-	 * @param aumentato Il primo parametro passato alla funzione <strong>Math.min()</strong>
-	 * @param riferimento Il secndo parametro passato alla funzione <strong>Math.min()</strong>
-	 * @return Il numero di tipo <code>double</code> estratto dalla funzione <em>Math.min()</em>
+	 * Esegue il return del <strong>numero minimo</strong> dei due passati come
+	 * parametri, usando la funzione <em>Math.min()</em>
+	 * 
+	 * @param aumentato   Il primo parametro passato alla funzione
+	 *                    <strong>Math.min()</strong>
+	 * @param riferimento Il secndo parametro passato alla funzione
+	 *                    <strong>Math.min()</strong>
+	 * @return Il numero di tipo <code>double</code> estratto dalla funzione
+	 *         <em>Math.min()</em>
 	 */
-	private double maxPunteggio(double aumentato, double riferimento ){
+	private double maxPunteggio(double aumentato, double riferimento) {
 
 		return Math.min(aumentato, riferimento);
 	}
 
 	/**
-	 * Esegue il return del <strong>numero massimo</strong> dei due passati
-	 *  come parametri, usando la funzione <em>Math.max()</em>
-	 *  
-	 * @param diminuito Il primo parametro passato alla funzione <strong>Math.max()</strong>
-	 * @param riferimento Il secndo parametro passato alla funzione <strong>Math.max()</strong>
-	 * @return Il numero di tipo <code>double</code> estratto dalla funzione <em>Math.max()</em>
+	 * Esegue il return del <strong>numero massimo</strong> dei due passati come
+	 * parametri, usando la funzione <em>Math.max()</em>
+	 * 
+	 * @param diminuito   Il primo parametro passato alla funzione
+	 *                    <strong>Math.max()</strong>
+	 * @param riferimento Il secndo parametro passato alla funzione
+	 *                    <strong>Math.max()</strong>
+	 * @return Il numero di tipo <code>double</code> estratto dalla funzione
+	 *         <em>Math.max()</em>
 	 */
-	private double minPunteggio( double diminuito, double riferimento ){
+	private double minPunteggio(double diminuito, double riferimento) {
 
 		return Math.max(diminuito, riferimento);
 	}
-	
+
 	/**
-	 * Questo metodo controlla che i valori (sazietà e soddisfazione) 
-	 * impostati durante la creazione del tamagotchi siano accettabili
+	 * Questo metodo controlla che i valori (sazietà e soddisfazione) impostati
+	 * durante la creazione del tamagotchi siano accettabili
 	 * 
-	 * @return ritorna un valore di tipo <code>boolean</code> che è 
-	 * <em>vero</em> se i valori sono accettabili, <em>falso</em> altrimenti.
+	 * @return ritorna un valore di tipo <code>boolean</code> che è <em>vero</em> se
+	 *         i valori sono accettabili, <em>falso</em> altrimenti.
 	 */
 	private boolean controlloValori() {
-		
-		return this.gradoSazieta >= MIN_SAZIETA && this.gradoAffettivo >= MIN_SODDISFAZIONE && this.gradoSazieta <= MAX_SAZIETA && this.gradoAffettivo <= MAX_SODDISFAZIONE;
+
+		return this.gradoSazieta >= MIN_SAZIETA && this.gradoAffettivo >= MIN_SODDISFAZIONE
+				&& this.gradoSazieta <= MAX_SAZIETA && this.gradoAffettivo <= MAX_SODDISFAZIONE;
 	}
-	
+
 	/**
-	 * Questo metodo viene richiamato dalla classe <a href="TamaMain.html">TamaMain</a>
-	 * quando l'utente vuole dare delle carezze al <em>Tamagotchi</em>
+	 * Questo metodo viene richiamato dalla classe
+	 * <a href="TamaMain.html">TamaMain</a> quando l'utente vuole dare delle carezze
+	 * al <em>Tamagotchi</em>
 	 */
-	public void riceviCarezze( ) {
+	public void riceviCarezze() {
 
 		int n = getRandomInt();
 
-		this.gradoAffettivo +=  n;
-		
+		this.gradoAffettivo += n;
+
 		decrementaSazieta(n);
 
 		this.gradoAffettivo = maxPunteggio(this.gradoAffettivo, MAX_SODDISFAZIONE);
 		this.gradoSazieta = minPunteggio(this.gradoSazieta, MIN_SAZIETA);
 	}
-	
-	/**
-	 * Questo metodo viene richiamato dalla classe <a href="TamaMain.html">TamaMain</a>
-	 * quando l'utente vuole dare delle carezze al <em>Tamagotchi</em>
-	 * @param n Questo paramentro indica il numero di carezze che l'utente vuole dare al proprio <em>Tamagotchi</em>
-	 */
-	public void riceviCarezze( int n ) {
 
-		this.gradoAffettivo +=  n;
-		
+	/**
+	 * Questo metodo viene richiamato dalla classe
+	 * <a href="TamaMain.html">TamaMain</a> quando l'utente vuole dare delle carezze
+	 * al <em>Tamagotchi</em>
+	 * 
+	 * @param n Questo paramentro indica il numero di carezze che l'utente vuole
+	 *          dare al proprio <em>Tamagotchi</em>
+	 */
+	public void riceviCarezze(int n) {
+
+		this.gradoAffettivo += n;
+
 		decrementaSazieta(n);
 
 		this.gradoAffettivo = maxPunteggio(this.gradoAffettivo, MAX_SODDISFAZIONE);
 		this.gradoSazieta = minPunteggio(this.gradoSazieta, MIN_SAZIETA);
 	}
-	
+
 	/**
-	 * Questo metodo viene richiamato dalla classe <a href="TamaMain.html">TamaMain</a>
-	 * quando l'utente vuole nutrire il <em>Tamagotchi</em>
+	 * Questo metodo viene richiamato dalla classe
+	 * <a href="TamaMain.html">TamaMain</a> quando l'utente vuole nutrire il
+	 * <em>Tamagotchi</em>
 	 */
-	public void riceviBiscotti( ) {
+	public void riceviBiscotti() {
 
 		int n = getRandomInt();
-		
-		for( int i = 0; i < n; i++ ) {
-			
-			this.gradoSazieta += (this.gradoSazieta*0.1);
+
+		for (int i = 0; i < n; i++) {
+
+			this.gradoSazieta += (this.gradoSazieta * 0.1);
 		}
-		
+
 		decrementaSoddisfazione(n);
 
 		this.gradoSazieta = maxPunteggio(this.gradoSazieta, MAX_SAZIETA);
@@ -172,58 +192,62 @@ public class Tamagotchi {
 	}
 
 	/**
-	 * Questo metodo viene richiamato dalla classe <a href="TamaMain.html">TamaMain</a>
-	 * quando l'utente vuole nutrire il <em>Tamagotchi</em>
-	 * @param n Questo paramentro indica il numero di biscotti che l'utente vuole dare al proprio <em>Tamagotchi</em>
+	 * Questo metodo viene richiamato dalla classe
+	 * <a href="TamaMain.html">TamaMain</a> quando l'utente vuole nutrire il
+	 * <em>Tamagotchi</em>
+	 * 
+	 * @param n Questo paramentro indica il numero di biscotti che l'utente vuole
+	 *          dare al proprio <em>Tamagotchi</em>
 	 */
-	public void riceviBiscotti( int n ) {
-		
-		for( int i = 0; i < n; i++ ) {
-			
-			this.gradoSazieta += (this.gradoSazieta*0.1);
+	public void riceviBiscotti(int n) {
+
+		for (int i = 0; i < n; i++) {
+
+			this.gradoSazieta += (this.gradoSazieta * 0.1);
 		}
-		
+
 		decrementaSoddisfazione(n);
 
 		this.gradoSazieta = maxPunteggio(this.gradoSazieta, MAX_SAZIETA);
 		this.gradoAffettivo = minPunteggio(this.gradoAffettivo, MIN_SODDISFAZIONE);
 	}
-	
+
 	/**
 	 * Metodo per decrementare la sazietà
+	 * 
 	 * @param n tirona il nuovo valore di sazietà decrementato
 	 */
 	public void decrementaSazieta(int n) {
-		
-		this.gradoSazieta -= (n/2);
+
+		this.gradoSazieta -= (n / 2);
 	}
-	
+
 	/**
 	 * Metodo per decrementare la soddisfazione
+	 * 
 	 * @param n tirona il nuovo valore di soddisfazione decrementato
 	 */
-	public void decrementaSoddisfazione( int n ) {
-		
-		this.gradoAffettivo -= (n/4);
+	public void decrementaSoddisfazione(int n) {
+
+		this.gradoAffettivo -= (n / 4);
 	}
-	
+
 	/**
 	 * Questa funzione viene utilizzate per controllare se il <em>Tamagotchi</em>
-	 *  felice o triste.
+	 * felice o triste.
 	 * 
-	 * @return Esegue un return di tipo <code>boolean</code> che sarà
-	 *  <em>vero</em> se il <em>Tamagotchi</em> risulta triste,
-	 *  <em>falso</em> se felice.
+	 * @return Esegue un return di tipo <code>boolean</code> che sarà <em>vero</em>
+	 *         se il <em>Tamagotchi</em> risulta triste, <em>falso</em> se felice.
 	 */
 	public boolean sonoTriste() {
-		
-		if( this.gradoSazieta <= TRISTEZZA_SAZIETA || this.gradoAffettivo <= TRISTEZZA_SODDISFAZIONE ) {
-			
+
+		if (this.gradoSazieta <= TRISTEZZA_SAZIETA || this.gradoAffettivo <= TRISTEZZA_SODDISFAZIONE) {
+
 			return true;
 		}
-		
-		if( this.gradoSazieta >= TRISTEZZA_SAZIETA_MAX ) {
-			
+
+		if (this.gradoSazieta >= TRISTEZZA_SAZIETA_MAX) {
+
 			return true;
 		}
 
@@ -231,45 +255,47 @@ public class Tamagotchi {
 	}
 
 	/**
-	 * Questa funzione viene utilizzate per controllare se il <em>Tamagotchi</em>
-	 *  è morto.
+	 * Questa funzione viene utilizzate per controllare se il <em>Tamagotchi</em> è
+	 * morto.
 	 * 
-	 * @return Esegue un return di tipo <code>boolean</code> che sarà
-	 *  <em>vero</em> se il <em>Tamagotchi</em> risulta morto,
-	 *  <em>falso</em> se vivo.
+	 * @return Esegue un return di tipo <code>boolean</code> che sarà <em>vero</em>
+	 *         se il <em>Tamagotchi</em> risulta morto, <em>falso</em> se vivo.
 	 */
-	public boolean sonoMorto( ) {
+	public boolean sonoMorto() {
 
-		if(this.gradoAffettivo == 0 || this.gradoSazieta == 0 || this.gradoSazieta == MAX_SAZIETA){
+		if (this.gradoAffettivo == 0 || this.gradoSazieta == 0 || this.gradoSazieta == MAX_SAZIETA) {
 
 			return true;
 		}
 
 		return false;
 	}
-	 
+
 	/**
-	 * Questo metodo produce una stringa che rappresenta lo stato del tamagotchi in funzione del grado di benessere generale
+	 * Questo metodo produce una stringa che rappresenta lo stato del tamagotchi in
+	 * funzione del grado di benessere generale
+	 * 
 	 * @return ritorna la stringa che visualizza l'espressione
 	 */
 	public String felicita() {
-		
-		if( this.gradoSazieta < TRISTEZZA_SAZIETA_MAX && this.gradoSazieta > TRISTEZZA_SAZIETA && this.gradoAffettivo > TRISTEZZA_SODDISFAZIONE) {
-			
+
+		if (this.gradoSazieta < TRISTEZZA_SAZIETA_MAX && this.gradoSazieta > TRISTEZZA_SAZIETA
+				&& this.gradoAffettivo > TRISTEZZA_SODDISFAZIONE) {
+
 			return FELICE;
-		}
-		else if( this.gradoSazieta < TRISTEZZA_SAZIETA_MAX && this.gradoSazieta > TRISTEZZA_SAZIETA && this.gradoAffettivo > TRISTEZZA_SODDISFAZIONE) {
-			
+		} else if (this.gradoSazieta < TRISTEZZA_SAZIETA_MAX && this.gradoSazieta > TRISTEZZA_SAZIETA
+				&& this.gradoAffettivo > TRISTEZZA_SODDISFAZIONE) {
+
 			return INDIFFERENTE;
-		}
-		else {
-			
+		} else {
+
 			return TRISTE;
 		}
 	}
 
 	/**
 	 * Metodo per ottenere il grado affettivo di un tamagotchi
+	 * 
 	 * @return ritorna il grado affettivo del tamagotchi
 	 */
 	public double getGradoAffettivo() {
@@ -278,6 +304,7 @@ public class Tamagotchi {
 
 	/**
 	 * Metodo per modificare il grado affettivo di un tamagotchi
+	 * 
 	 * @param gradoAffettivo nuovo grado affettivo del tamagotchi
 	 */
 	public void setGradoAffettivo(double gradoAffettivo) {
@@ -286,6 +313,7 @@ public class Tamagotchi {
 
 	/**
 	 * Metodo per ottenere il grado di sazietà di un tamagotchi
+	 * 
 	 * @return ritorna il grado di sazietà del tamagotchi
 	 */
 	public double getGradoSazieta() {
@@ -294,6 +322,7 @@ public class Tamagotchi {
 
 	/**
 	 * Metodo per modificare il grado di sazietà
+	 * 
 	 * @param gradoSazieta nuovo grado di sazietà
 	 */
 	public void setGradoSazieta(double gradoSazieta) {
@@ -304,14 +333,16 @@ public class Tamagotchi {
 	 * Restituisce una rappresentazione leggibile di un oggetto della classe
 	 * <strong>Tamagotchi</strong>
 	 * 
-	 * @return Una <code>String</co de> che rappresenta un <strong>Tamagotchi</strong>
+	 * @return Una <code>String</co de> che rappresenta un
+	 *         <strong>Tamagotchi</strong>
 	 */
 	public String toString() {
 
+		String s0 = this.getNome();
 		String s1 = String.format("Sazietà: %.2f Soddisfazione: %.2f", this.gradoSazieta, this.gradoAffettivo);
 		String s2 = String.format(felicita());
-		
-		return BelleStringhe.incorniciaECentra( s1 ) + BelleStringhe.centrata( s2 );
+
+		return BelleStringhe.centrata(s0) + '\n' + BelleStringhe.incorniciaECentra(s1) + BelleStringhe.centrata(s2);
 	}
 
 }
